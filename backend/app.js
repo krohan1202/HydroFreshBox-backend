80e129e9375c4971b56952b82afebaf31622dced
Rohan Karmakar <krohan1202@gmail.com>
2021-01-26 22:21:42 +0530

    Add routes, controllers, models folder

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//My Routes
+const newsletter = require("./routes/newsletterEmail");
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED!!");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes
+app.use("/", newsletter);
+
+app.listen(process.env.PORT || 8000, () => console.log("Server connected!"));
