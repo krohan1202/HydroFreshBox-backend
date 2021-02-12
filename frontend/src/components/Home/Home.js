0a3d12582ddbccd42fd60fdb6ac00f1c7bb6f6be
Rohan Karmakar <krohan1202@gmail.com>
2021-02-12 18:30:34 +0530

    Add sweet sans regular & update footer

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -148,6 +148,35 @@ function Home() {
                 <img src={FooterBg} className="footer__bg" />
 
                 <p className="footer__Heading">Hydro Fresh Box</p>
+                <span className="footer__NavigatePart">
+                    <ul>
+                    <li className="footer__NavigatePart--Heading">Navigate</li>
+                        <li className="footer__NavigatePart--links"><a href="#" className="footer__NavigatePart--aboutUs">About Us</a></li>
+                        <li className="footer__NavigatePart--links"><a href="#" className="footer__NavigatePart--shop">Shop</a></li>
+                        <li className="footer__NavigatePart--links"><a href="#" className="footer__NavigatePart--viewPods">View Pods</a></li>
+                        <li className="footer__NavigatePart--links"><a href="#" className="footer__NavigatePart--contactUs">Contact Us</a></li>
+                    </ul>
+                </span>
+                <span className="footer__NeedHelpPart">
+                    <ul>
+                    <li className="footer__NeedHelpPart--Heading">Need Help?</li>
+                        <li className="footer__NeedHelpPart--links"><a href="#" className="footer__NeedHelpPart--customerService">Customer Service</a></li>
+                        <li className="footer__NeedHelpPart--links"><a href="#" className="footer__NeedHelpPart--locations">Locations</a></li>
+                        <li className="footer__NeedHelpPart--links"><a href="#" className="footer__NeedHelpPart--news">News</a></li>
+                        <li className="footer__NeedHelpPart--links"><a href="#" className="footer__NeedHelpPart--emailUs">Email Us</a></li>
+                    </ul>
+                </span>
+                <span className="footer__FollowUsPart">
+                    <ul>
+                    <li className="footer__FollowUsPart--Heading">Follow us</li>
+                        <li className="footer__FollowUsPart--links"><a href="#" className="footer__FollowUsPart--fb">Facebook</a></li>
+                        <li className="footer__FollowUsPart--links"><a href="#" className="footer__FollowUsPart--insta">Instagram</a></li>
+                    </ul>
+                </span>
+                <hr className="footer__horizontalLine"></hr>
+                <p className="footer__termsAndConditions">Terms & Conditions</p>
+                <p className="footer__rightsReserved">All rights reserved — 2021 © Hydro Fresh Box</p>
+                <p className="footer__privacyPolicy">Privacy Policy</p>
             </div>
         </>
     )
